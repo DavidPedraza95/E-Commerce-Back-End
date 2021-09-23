7c86a749cdf5050dc724b9deb7dd948f75f699d5
David Pedraza <82422713+DavidPedraza95@users.noreply.github.com>
2021-09-23 13:48:51 -0400

    adding initial files

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product],
+  })
+    .then((dbRes) => {
+      res.json(dbRes);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((dbRes) => {
+      res.json(dbRes);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(req.body)
+  .then((dbRes) => {
+    res.json(dbRes);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbRes) => {
+      res.json(dbRes);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbRes) => {
+      res.json(dbRes);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
